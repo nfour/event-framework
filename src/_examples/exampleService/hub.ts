9f9ac34bd09263aac03eb5dfb5d927106d0b36c1
nfour <novus.nfour@gmail.com>
2018-01-09 18:47:28 +1000

    how it werks

--- src/_examples/exampleService/hub.ts
+++ src/_examples/exampleService/hub.ts
@@ -19,22 +19,3 @@ setInterval(async () => {
     }),
   });
 }, 2000);
-
-/**
- * How it works:
- *
- * (HttpServer input)
- * - hub emits a `start` event
- * - httpServer listens on that, then starts its server up
- * - http server has an endpoint routed to action `foo`
- * - we send a fetch request to that endpoint
- * - httpServer passes a HttpRequestEvent event to `foo`
- * - middleware components catch it, turn it into an `execute` event (the only thing actions understand)
- * - it executes
- * - the result is used as a response to the incoming request in httpServer
- *
- * (HttpLambda input)
- * - HttpLambda is instantiated with an Action, `foo`
- * - HttpLambda.handler() returns a lambda handler that emits a `HttpRequestEvent` to `foo`
- * - middlewares execute `foo` and respond
- */
